bb80c8633cd3ffe052b544acf87a73656c79cc4f
Francesco Di Blasi <franzdblasi@gmail.com>
2024-04-14 23:54:31 +0200

    removed

--- src/omnilight.gd
+++
@@ -1,8 +0,0 @@
-@tool
-class_name OmniCustom extends OmniLight3D
-
-
-func _set(property: StringName, value: Variant) -> bool:
-	if property == "light_energy":
-		notify_property_list_changed()
-	return true
